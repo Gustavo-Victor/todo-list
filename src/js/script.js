af86c488e7914898907905a3f0179f03a0bddf52
Gustavo Souza <gustavovbs270@gmail.com>
2022-12-08 20:24:43 -0300

    Finish project

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,85 @@
-// Elements selection
+// Elements selection / variables
 const todoform = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const todolist = document.querySelector("#todo-list");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#search-input");
+const filterSelect = document.querySelector('#filter-select');
 let oldInputValue;
+let database = {items: []};
 
 
 // Functions 
-const saveTodo = (text) => {
-    todolist.innerHTML += `
-    <div class="todo">
-        <h3>${text}</h3>
-        <button class="finish-todo">
-            <i class="fa-solid fa-check"></i>
-        </button>
-        <button class="edit-todo">
-            <i class="fa-solid fa-pen"></i>
-        </button>
-        <button class="delete-todo">
-            <i class="fa-solid fa-xmark"></i>
-        </button>
-    </div>
-    `;
+const setData = (data) => {
+    localStorage.setItem('database', JSON.stringify(data));
+}
+
+const getData = () => {
+    return JSON.parse(localStorage.getItem('database')) || {items:[]};
+}
+
+const createTodo = () => {
+    database = getData();
+    const items = database.items; 
+    displayTodo(items);
+}
+
+const displayTodo = (items) => {
+    todolist.innerHTML = '';
+    if(items.length > 0) {
+        items.forEach((item, index) => {            
+            let div = document.createElement('div');
+            
+            if(item.status == 'done') {
+                div.setAttribute('class', 'done');
+            }else {
+                div.removeAttribute('class', 'done');
+            }
+            div.setAttribute('id', 'id'+index); 
+                        
+            div.innerHTML = `
+                <h3>${item.title}</h3>
+                <div class='buttons'>
+                <button class="finish-todo">
+                <i class="fa-solid fa-check"></i>
+                </button>
+                <button class="edit-todo">
+                    <i class="fa-solid fa-pen"></i>
+                </button>
+                <button class="delete-todo">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
+                </div>
+            `;
+
+            if(!div.classList.contains('todo')){
+                div.classList.add('todo');
+            }
+            todolist.append(div);            
+        })
+    }
+}
+
+const getTodoId = (text) => {
+    database = getData();
+    let myData = {};
+    database.items.filter(item => {
+       if(item.title == text) {
+            myData = item;
+            return;
+       }
+    });  
+    return database.items.indexOf(myData);
+}
+
+const removeTodo = (text) => {
+    let id = getTodoId(text);  
+    console.log(id);
+    database.items.splice(id, 1);
+    setData(database);  
+    displayTodo(database.items);
 }
 
 const toggleForm = () => {
@@ -31,53 +87,75 @@ const toggleForm = () => {
     todoform.classList.toggle('hide');
 }
 
-const updateTodo = (text) => {
-    const allTodos = document.querySelectorAll('.todo');
-
-    allTodos.forEach(todo => {
-        let title = todo.querySelector('h3');
-        if(title.innerText === oldInputValue){
-            title.innerText = text;            
-        }
-    });
+const updateTodo = (text) => {   
+    database = getData(); 
+    let id = getTodoId(oldInputValue);           
+     database.items[id].title = text;
+     setData(database);
+     displayTodo(database.items);
+     console.log(id);   
 }
+
+const updateTodoStatus = (text) => {
+    let id = getTodoId(text);
+    console.log(id);
+    let st = database.items[id].status;
+    if(st == 'todo') {
+        st = 'done';
+    }else {
+        st = 'todo';
+    }
+    database.items[id].title = text;
+    database.items[id].status = st;
+    console.log(database.items[id]);
+    setData(database);    
+    displayTodo(database.items);
+}
+
 
 // Events
 todoform.addEventListener("submit", (e) => {
     e.preventDefault();
+    
     const inputValue = todoInput.value;
+    console.log(todoInput.id);
     
     if(inputValue.length == 0) {
         console.error("Preencha o campo...");
     } else {
-        console.log("Tudo ok");
-        saveTodo(inputValue);
         todoInput.value = '';
+        database.items.push({title: inputValue, status: 'todo'});
+        setData(database);
+        createTodo();
     }   
 });
 
 
 todolist.addEventListener('click', (e) => {
     const targetEl = e.target;
-    const parentTodo= targetEl.parentElement;
+    const buttonsDiv = targetEl.parentElement;
+    const titleSibling = buttonsDiv.previousElementSibling;
+    const parentTodo = titleSibling.parentElement;
     let todoTitle;
 
-    if(parentTodo && parentTodo.querySelector('h3')) {
-        todoTitle = parentTodo.querySelector('h3');
+    console.log(targetEl, buttonsDiv, titleSibling, parentTodo);
+
+    if(parentTodo && titleSibling) {
+        todoTitle = titleSibling.innerText;
     }
     
     if(targetEl.classList.contains('finish-todo')){ 
-        parentTodo.classList.toggle('done');
+        updateTodoStatus(todoTitle);
     }
 
     if(targetEl.classList.contains('edit-todo')){
-        editInput.value = todoTitle.innerText;    
-        oldInputValue = todoTitle.innerText;
+        editInput.value = todoTitle;    
+        oldInputValue = todoTitle;
         toggleForm();
     }
 
-    if(targetEl.classList.contains('delete-todo')){
-        todolist.removeChild(parentTodo);
+    if(targetEl.classList.contains('delete-todo')){        
+        removeTodo(todoTitle);               
     }
     
 });
@@ -89,12 +167,53 @@ cancelEditBtn.addEventListener('click', (e) => {
 });
 
 editForm.addEventListener('submit', (e) => {
-    e.preventDefault();    
+    e.preventDefault();     
     
     const editInputValue = editInput.value; 
     if(editInputValue) {
         updateTodo(editInputValue);
     }
-    console.log('editado com sucesso!');
     toggleForm();
 });
+
+searchInput.addEventListener('input', (e) => {    
+    e.preventDefault();
+    let searchValue = searchInput.value;
+    database = getData();
+    const filteredItems = [];
+    
+    database.items
+    .filter(item => {
+        return item.title.toLowerCase().includes(searchValue.toLowerCase());         
+    })
+    .forEach(filteredItem => {
+        console.log(filteredItem);
+        filteredItems.push(filteredItem);
+    });
+    displayTodo(filteredItems);
+});
+
+filterSelect.addEventListener('change', (e) => {
+    e.preventDefault();
+    database = getData();
+    let selectValue = e.target.value;
+    let myFilteredArray = [];
+
+    switch(selectValue) {
+        case 'todo':
+            myFilteredArray = database.items.filter(item => item.status == 'todo');
+        break;
+
+        case 'done': 
+            myFilteredArray = database.items.filter(item => item.status == 'done');
+        break;
+
+        default: 
+            myFilteredArray = database.items;
+    }
+    // console.log(myFilteredArray);
+    displayTodo(myFilteredArray);
+})
+
+//check database and update information
+createTodo();
